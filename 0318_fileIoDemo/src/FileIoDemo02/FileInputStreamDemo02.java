9c5d9f9aa8e8e983e99fdd58edd024916dc894d7
Doing <helong@yuelu.net>
2018-03-19 15:52:47 +0800

    20180319

--- 0318_fileIoDemo/src/FileIoDemo02/FileInputStreamDemo02.java
+++ 0318_fileIoDemo/src/FileIoDemo02/FileInputStreamDemo02.java
@@ -11,5 +11,37 @@ import java.io.FileNotFoundException;
 public class FileInputStreamDemo02 {
 	public static void main(String[] args) throws Exception {
 		FileInputStream fis = new FileInputStream("F:\\a\\b.txt");
+		// 创建字节数组
+		byte[] b = new byte[2];
+		int len = fis.read(b);
+
+		// 调用字节的String方法
+		System.out.println(new String(b));
+		System.out.println(len);
+
+		len = fis.read(b);
+
+		// 调用字节的String方法
+		System.out.println(new String(b));
+		System.out.println(len);
+
+		len = fis.read(b);
+
+		// 调用字节的String方法
+		System.out.println(new String(b));
+		System.out.println(len);
+
+		len = fis.read(b);
+
+		// 调用字节的String方法
+		System.out.println(new String(b));
+		System.out.println(len);
+		
+		len = fis.read(b);
+
+		// 调用字节的String方法
+		System.out.println(new String(b));
+		System.out.println(len);
+		// abcdefg
 	}
 }
