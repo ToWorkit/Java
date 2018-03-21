38b229cb3e15c293f18a78da21afdb02625b8988
Doing <helong@yuelu.net>
2018-03-21 09:50:43 +0800

    20180321

--- 0318_fileIoDemo/src/demo08/BufferedInputStreamDemo01.java
+++ 0318_fileIoDemo/src/demo08/BufferedInputStreamDemo01.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /*
+ * 读取数据
  * 缓冲流
  * 	提高程序的执行效率
  * 	写入数据到流中，字节缓冲输入流，BufferedInputStream
@@ -13,7 +14,7 @@ import java.io.IOException;
 public class BufferedInputStreamDemo01 {
 	public static void main(String[] args) throws IOException {
 		// 创建缓冲流
-		// 匿名对象的形式将输入流作为参数传入
+		// 匿名对象的形式将输入流(读取数据)作为参数传入
 		BufferedInputStream bis = new BufferedInputStream(new FileInputStream("F:\\a\\b.txt"));
 		
 		// 字节数组形式写入，长度为1024(每次读取1024)
