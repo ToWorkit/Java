a7bb4961ae2eeb44e9ed7a28db8656da7f9f65bd
Doing <helong@yuelu.net>
2018-03-23 15:45:23 +0800

    20180323

--- 0319/src/demo05/OutThread.java
+++ 0319/src/demo05/OutThread.java
@@ -1,5 +1,25 @@
 package demo05;
 
-public class OutThread {
+public class OutThread implements Runnable{
+	private Resource r;
 
+	@Override
+	public void run() {
+		while(true) {
+			synchronized (r) {
+				// 判断标记false等待
+				if (!r.flag) {
+					try {
+						r.wait();
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(r.name + "..." + r.sex);
+				
+				// 标记为 false 唤醒对方线程
+			}
+		}
+	}
+	
 }
