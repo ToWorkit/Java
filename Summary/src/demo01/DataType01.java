5dccd3fd0dfb544c36da58e342cc7f59d84c44e5
Toworkit <1211126374@qq.com>
2018-03-25 12:08:36 +0800

    20180325

--- Summary/src/demo01/DataType01.java
+++ Summary/src/demo01/DataType01.java
@@ -3,8 +3,21 @@ package demo01;
 public class DataType01 {
 	public static void main(String[] args) {
 		// 整数类型
-		byte b = 10; // 占一个字节 -128 ~ 127
-		int i = 10; // 占两个字节 -32768 ~ 32767
+		byte b = 10; // 占 一 个字节 -128 ~ 127
+		int i = 10; // 占 两 个字节 -32768 ~ 32767，默认
+		short s = 10; // 占 两 个字节 -2147483648 ~ 2147483648
+		long l = 999999999999L; // 占 八 个字节 -9223372036854775808 ~ 9223372036854775807
+		
+		// 浮点型
+		float f = 12.3F; // 占 四 个字节
+		double d = 33.4; // 占 八 个字节 默认
+		
+		// 字符类型 占 两 个字节
+		char c = 'a'; 
+		
+		// 布尔类型  占 两 个字节
+		boolean b1 = true;
+		boolean b2 = false;
 		
 	}
 }
---
+++ Summary/src/demo01/Interview_byte.java
@@ -0,0 +1,14 @@
+package demo01;
+
+public class Interview_byte {
+	public static void main(String[] args) {
+		byte b1 = 3;
+		byte b2 = 4;
+		
+		byte b3 = (byte) (b1 + b2);
+		System.out.println(b3);
+		
+		byte b4 = 3 + 4;
+		System.out.println(b4);
+	}
+}
