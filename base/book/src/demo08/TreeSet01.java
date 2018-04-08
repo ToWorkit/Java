4703f0b12a78d540814a63df70a8c6018773916b
Doing <1211126374@qq.com>
2018-04-08 18:02:16 +0800

    20180408

--- base/book/src/demo08/TreeSet01.java
+++ base/book/src/demo08/TreeSet01.java
@@ -1,5 +1,7 @@
 package demo08;
 
+import java.util.Comparator;
+
 /*
 TreeSet集合是用来对象元素进行排序的,同样他也可以保证元素的唯一
 	 * 当compareTo方法返回0的时候集合中只有一个元素
@@ -9,6 +11,19 @@ TreeSet集合是用来对象元素进行排序的,同样他也可以保证元素的唯一
 
 public class TreeSet01 {
 	public static void main(String[] args) {
-		
+		// 将字符串按照长度排序
+//		TreeSet<String> ts = new TreeSet<>(new )
 	}
+}
+
+// 比较
+class CompareByLen implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		// 按照字符串的长度比较
+		int num = s1.length() - s2.length();
+		return num == 0 ? s1.compareTo(s2) : num;
+	}
+	
 }
