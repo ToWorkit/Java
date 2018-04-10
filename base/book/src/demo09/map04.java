6a97602ffefed42bb89a8db1118e144bad1060f0
Doing <1211126374@qq.com>
2018-04-10 10:43:44 +0800

    20180410

--- base/book/src/demo09/map04.java
+++ base/book/src/demo09/map04.java
@@ -6,13 +6,31 @@ import java.util.TreeMap;
 public class map04 {
 	public static void main(String[] args) {
 		TreeMap<Student, String> tm = new TreeMap<>(new Comparator<Student>() {
-
 			@Override
-			public int compare(Student o1, Student o2) {
-				// TODO Auto-generated method stub
-				return 0;
+			public int compare(Student s1, Student s2) {
+				// 按照姓名比较
+				int num = s1.getName().compareTo(s2.getName());
+				return num == 0 ? s1.getAge() - s2.getAge() : num;
 			}
-			
 		});
+		tm.put(new Student("b", 22), "中国");
+		tm.put(new Student("d", 37), "国外");
+		tm.put(new Student("c", 20), "地球");
+		tm.put(new Student("a", 18), "出去");
+		// 姓名排序
+		System.out.println(tm);
+		System.out.println("-------------");
+		demo01();
+	}
+	
+	public static void demo01() {
+		TreeMap<Student, String> tm = new TreeMap<>();
+		tm.put(new Student("b", 22), "中国");
+		tm.put(new Student("d", 37), "国外");
+		tm.put(new Student("a", 16), "出去");
+		tm.put(new Student("c", 20), "地球");
+		tm.put(new Student("a", 18), "出去");
+		// 年龄排序
+		System.out.println(tm);
 	}
 }
