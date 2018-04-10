b3b7101b1194f4b16fc17c2e16935190e238fbec
Doing <1211126374@qq.com>
2018-04-10 16:48:40 +0800

    20180410

---
+++ base/book/src/demo12/test01.java
@@ -0,0 +1,39 @@
+package demo12;
+
+/**
+ * * A:面试题1
+		* final,finally和finalize的区别
+		* final可以修饰类,不能被继承
+		* 修饰方法,不能被重写
+		* 修饰变量,只能赋值一次
+		* 
+		* finally是try语句中的一个语句体,不能单独使用,用来释放资源
+		* 
+		* finalize是一个方法,当垃圾回收器确定不存在对该对象的更多引用时，由对象的垃圾回收器调用此方法。
+	* B:面试题2
+		* 如果catch里面有return语句，请问finally的代码还会执行吗?如果会，请问是在return前还是return后。
+ */
+
+public class test01 {
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		System.out.println(d.method());
+	}
+}
+class Demo {
+	public int method() {
+		int x = 10;
+		try {
+			x = 20;
+			System.out.println(1/0);
+			return x;
+		} catch (Exception e) {
+			x = 30;
+			return x;
+		} finally {
+			x = 40;
+			// 必然会执行，值为 40
+//			return x; 
+		}
+	}
+}
