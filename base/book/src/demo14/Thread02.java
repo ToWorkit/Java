b1d9192404bb21a21e13bc11bc5992dc8f26f33c
Doing <1211126374@qq.com>
2018-04-11 17:18:29 +0800

    20180411

--- base/book/src/demo14/Thread02.java
+++ base/book/src/demo14/Thread02.java
@@ -1,5 +1,16 @@
 package demo14;
 
 public class Thread02 {
+	public static void main(String[] args) {
+		
+	}
+}
 
+// 1 定义一个类实现Runnable
+class MyRunnable implements Runnable {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
