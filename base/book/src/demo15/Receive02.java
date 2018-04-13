b83b20037dc3589fb8232eeec96731d8bb23bb50
Doing <1211126374@qq.com>
2018-04-13 14:03:07 +0800

    20180413

--- base/book/src/demo15/Receive02.java
+++ base/book/src/demo15/Receive02.java
@@ -1,5 +1,8 @@
 package demo15;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /*
  * 2 接收Receive
  * 创建DatagramSocket，指定端口号
@@ -10,7 +13,25 @@ package demo15;
  */
 
 public class Receive02 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		// 创建Socket -> 码头
+		DatagramSocket socket = new DatagramSocket(9090);
+		// 创建Packet -> 集装箱
+		DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
 		
+		while(true) {
+			// 接收数据(send发来的数据货物)
+			socket.receive(packet);
+			// 获取数据
+			byte[] arr = packet.getData();
+			// 获取有效的字节个数
+			int len = packet.getLength();
+			// 获取ip地址
+			String ip = packet.getAddress().getHostAddress();
+			// 获取端口号
+			int port = packet.getPort();
+			// 获取端口号
+			System.out.println(ip + ":" + port + ":" + new String(arr, 0, len));
+		}
 	}
 }
