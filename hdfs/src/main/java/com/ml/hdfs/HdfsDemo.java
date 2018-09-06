d1ef7e0a6c32650c9cff53ce6798a20b323c8afe
Toworkit <1211126374@qq.com>
2018-09-07 07:30:24 +0800

    update

--- hdfs/src/main/java/com/ml/hdfs/HdfsDemo.java
+++ hdfs/src/main/java/com/ml/hdfs/HdfsDemo.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.IOUtils;
 import org.junit.Test;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -152,14 +153,32 @@ public class HdfsDemo {
     public void fileIoPut() throws Exception {
         // 获取hdfs的客户端
         Configuration configuration = new Configuration();
-        FileSystem fileSystem = FileSystem.get(new URI("hdfs://bigdata111"), configuration, "ml");
+        FileSystem fileSystem = FileSystem.get(new URI("hdfs://bigdata111:9000"), configuration, "ml");
         // 创建输入流
         FileInputStream fis = new FileInputStream("d:/test.txt");
         // 创建输出流
-        FSDataOutputStream fos = fileSystem.create(new Path("/9090"));
+        FSDataOutputStream fos = fileSystem.create(new Path("/9090/test_02.txt"));
         // 流的传输
         IOUtils.copyBytes(fis, fos, configuration);
         // 关闭资源
+        fileSystem.close();
+    }
+
+    /**
+     * IO流下载文件
+     */
+    @Test
+    public void fileDown() throws Exception {
+        // 获取hdfs客户端
+        Configuration configuration = new Configuration();
+        FileSystem fileSystem = FileSystem.get(new URI("hdfs://bigdata111:9000"), configuration, "ml");
+        // 获取输入流
+        FSDataInputStream fis = fileSystem.open(new Path("/9090/test_02.txt"));
+        // 获取输出流
+        FileOutputStream fos = new FileOutputStream("d:/test_03.txt");
+        // 流的传递
+        IOUtils.copyBytes(fis, fos, configuration);
+        // 关闭
         fileSystem.close();
     }
 }
