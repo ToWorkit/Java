2137dbe2a4d5750a7333488ea2735a2372aedbdf
Doing <1211126374@qq.com>
2018-09-20 11:45:33 +0800

    update

---
+++ note/src/com/CollectionP/Collection_02.java
@@ -0,0 +1,23 @@
+package com.CollectionP;
+
+public class Collection_02 {
+    public static void main(String[] args) {
+        /**
+         * Collection
+         *
+         * List 有序集合，有索引，存与取的顺序一样，可以重复
+         *      ArrayList
+         *          数组实现
+         *      LinkedList
+         *          链表实现
+         *      Vector
+         *          数组实现
+         *
+         * Set  无序集合，无索引，存与取的顺序不一样，不可以重复
+         *      HashSet
+         *          哈希算法
+         *      TreeSet
+         *          二叉树算法
+         */
+    }
+}
