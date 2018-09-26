7f060e33a8390d9403c3d23cab85055ebef9c07b
Doing <1211126374@qq.com>
2018-09-26 09:40:20 +0800

    update

--- note/src/com/interfaceP/interface_02.java
+++ note/src/com/interfaceP/interface_02.java
@@ -17,7 +17,10 @@ public class interface_02 {
         for (int i = 0; i < list.size(); i ++) {
             if (list.get(i).equals("c")) {
                 // 删除元素(只能删除一个)
+                // 因为删除后 i(查找索引)没有变 但是数据的索引往前进了一步
                 list.remove("c");
+                // 索引减一解决
+                i--;
             }
         }
         System.out.println(list);
